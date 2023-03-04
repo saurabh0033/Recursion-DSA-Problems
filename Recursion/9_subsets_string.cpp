09a25ba2eb6be3c63127352f5f5b064ba3dd4eb5
Chavan Saurabh Ravindra <82364105+saurabh0033@users.noreply.github.com>
2023-03-04 08:19:01 +0530

    Update 9_subsets_string.cpp

--- Recursion/9_subsets_string.cpp
+++ Recursion/9_subsets_string.cpp
@@ -1,39 +1,34 @@
- #include<bits/stdc++.h>
- using namespace std;
- 
- void solve(vector<int>&v, vector<int>temp,int i,vector<vector<int>>&ans){
-     //base case
-     if(i>=v.size()){
-         ans.push_back(temp);
-         return ;
-     }
-     
-     //exclude call
-     solve(v,temp,i+1,ans);
-     
-     //inclusive call
-     int data=v[i];
-     temp.push_back(data);
-     solve(v,temp,i+1,ans);
- }
- 
- int main(){
-     cout<<"----------Subsets--------------------";
-     cout<<endl<<endl;
-     
-     vector<int>v;
-     v={1,2,3};
-     vector<int>temp;
-     vector<vector<int>>ans;
-     int i=0;
-     solve(v,temp,i,ans);
-     
-     for(int i=0;i<ans.size();i++){
-         cout<<"{";
-         for(int j=0;j<ans[i].size();j++){
-             cout<<ans[i][j]<<" ";
-         }
-         cout<<"}"<<endl;
-     }
-     return 0;
- }
+// CPP program to generate power set
+#include <bits/stdc++.h>
+using namespace std;
+
+// str : Stores input string
+// curr : Stores current subset
+// index : Index in current subset, curr
+void powerSet(string str, int index = 0, string curr = "")
+{
+	int n = str.length();
+
+	// base case
+	if (index == n) {
+		cout << curr << endl;
+		return;
+	}
+
+	// Two cases for every character
+	// (i) We consider the character
+	// as part of current subset
+	// (ii) We do not consider current
+	// character as part of current
+	// subset
+	powerSet(str, index + 1, curr + str[index]);
+	powerSet(str, index + 1, curr);
+}
+
+// Driver code
+int main()
+{
+	string str = "abc";
+	powerSet(str);
+	return 0;
+}
